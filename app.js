65bdb319c45351ff1c30113cf54edd2c19e90dce
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-10-05 14:40:30 +1000

    CORS changes

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const port = 3000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-
+app.use(function(req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
