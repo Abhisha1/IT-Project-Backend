27dcd08f007ad202627d0c571ae08df5ce8b5ce5
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-10-05 15:32:15 +1000

    cors and port changhes

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(function(req,res,next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-})
+});
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
